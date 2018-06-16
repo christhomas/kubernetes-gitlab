f01b960617408f5c93d56045abaabcfd787ad9f7
Chris Thomas <chris.thomas@antimatter-studios.com>
2018-06-16 15:33:10 +0200

    upgrade gitlab version and do some small updates

--- toggle_secrets.sh
+++ toggle_secrets.sh
@@ -2,18 +2,22 @@
 
 red=$'\e[1;31m'; grn=$'\e[1;32m'; end=$'\e[0m'
 
+function color_print() {
+    echo "$1$2${end}"
+}
+
 if [[ "$1" = "skip" ]] || [[ "$1" = "no-skip" ]]; then
     git update-index --$1-worktree *gitlab-secrets*
     git status
 
     if [[ "$1" = "skip" ]]; then
-        echo "${grn}File should be missing from git status${end}";
+        color_print ${grn} "File should be missing from git status"
     fi
 
     if [[ "$1" = "no-skip" ]]; then
-        echo "${red}FILE SHOULD BE IN STATUS LIST, BE CAREFUL!!${end}";
+        color_print ${red} "FILE SHOULD BE IN STATUS LIST, BE CAREFUL!!"
     fi
 else
-    echo "${red}First parameter can only be 'skip' or 'no-skip'${end}"
+    color_print ${red} "First parameter can only be 'skip' or 'no-skip'"
     exit 1
 fi
